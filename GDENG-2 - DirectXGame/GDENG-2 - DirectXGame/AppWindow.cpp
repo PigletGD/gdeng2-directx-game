34e18d8f76f81304345925799f90d006ad7541bc
Jacob Manzano <jacobmanzano2@gmail.com>
2021-07-04 01:07:47 +0800

    Initial Draw Triangles

--- GDENG-2 - DirectXGame/GDENG-2 - DirectXGame/AppWindow.cpp
+++ GDENG-2 - DirectXGame/GDENG-2 - DirectXGame/AppWindow.cpp
@@ -1,5 +1,8 @@
 #include "AppWindow.h"
 
+struct vec3 { float x, y, z; };
+struct vertex { vec3 position; };
+
 AppWindow::AppWindow()
 {
 }
@@ -17,12 +20,39 @@ void AppWindow::onCreate()
 
 	RECT rc = this->getClientWindowRect();
 	m_swap_chain->init(this->m_HWND, rc.right - rc.left, rc.bottom - rc.top);
+
+	vertex list[] = {
+		//x - y - z
+		{-0.5f,-0.5f, 0.0f}, // POS1
+		{ 0.0f, 0.5f, 0.0f}, // POS2
+		{ 0.5f,-0.5f, 0.0f}, // POS3
+	};
+
+	m_vb = GraphicsEngine::get()->createVertexBuffer();
+	UINT size_list = ARRAYSIZE(list);
+
+	GraphicsEngine::get()->createShaders();
+
+	void* shader_byte_code = nullptr;
+	UINT size_shader = 0;
+	GraphicsEngine::get()->getShaderBufferAndSize(&shader_byte_code, &size_shader);
+
+	m_vb->load(list, sizeof(vertex), size_list, shader_byte_code, size_shader);
 }
 
 void AppWindow::onUpdate()
 {
 	Window::onUpdate();
 	GraphicsEngine::get()->getImmediateDeviceContext()->clearRenderTargetColor(this->m_swap_chain, 1, 0, 0, 1);
+
+	RECT rc = this->getClientWindowRect();
+	GraphicsEngine::get()->getImmediateDeviceContext()->setViewportSize(rc.right - rc.left, rc.bottom - rc.top);
+
+	GraphicsEngine::get()->setShaders();
+
+	GraphicsEngine::get()->getImmediateDeviceContext()->setVertexBuffer(m_vb);
+
+	GraphicsEngine::get()->getImmediateDeviceContext()->drawTriangleList(m_vb->getSizeVertexList(), 0);
 
 	m_swap_chain->present(true);
 }
@@ -31,6 +61,8 @@ void AppWindow::onDestroy()
 {
 	Window::onDestroy();
 
+	m_vb->release();
 	m_swap_chain->release();
+
 	GraphicsEngine::get()->release();
 }
--- GDENG-2 - DirectXGame/GDENG-2 - DirectXGame/GraphicsEngine.h
+++ GDENG-2 - DirectXGame/GDENG-2 - DirectXGame/GraphicsEngine.h
@@ -3,6 +3,7 @@
 
 class SwapChain;
 class DeviceContext;
+class VertexBuffer;
 
 class GraphicsEngine
 {
@@ -15,6 +16,11 @@ public:
 
 	SwapChain* createSwapChain();
 	DeviceContext* getImmediateDeviceContext();
+	VertexBuffer* createVertexBuffer();
+
+	bool createShaders();
+	bool setShaders();
+	void getShaderBufferAndSize(void** bytecode, UINT* size);
 
 	static GraphicsEngine* get();
 
@@ -25,9 +31,16 @@ private:
 	IDXGIDevice* m_dxgi_device;
 	IDXGIAdapter* m_dxgi_adapter;
 	IDXGIFactory* m_dxgi_factory;
+	ID3D11DeviceContext* m_imm_context;
 
 	DeviceContext* m_imm_device_context;
 
+	ID3DBlob* m_vsblob = nullptr;
+	ID3DBlob* m_psblob = nullptr;
+	ID3D11VertexShader* m_vs = nullptr;
+	ID3D11PixelShader* m_ps = nullptr;
+
 	friend class SwapChain;
+	friend class VertexBuffer;
 };
 
